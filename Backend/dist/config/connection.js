c25a5b3c60dd5983714e28587f987644ce4411e1
Faris Abuali <Faris-Abuali@outlook.com>
2023-03-07 22:05:48 +0200

    Delete Backend/dist directory

--- Backend/dist/config/connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('test', 'root', 'haneen', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-exports.default = db;
--- Backend/dist/model/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const student_1 = require("./student");
-student_1.Student.sync().then(() => {
-    console.log("connect to db");
-});
