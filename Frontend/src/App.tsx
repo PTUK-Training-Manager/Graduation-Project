6fe7e461a28070c7cb35d94b13ece47f467b6d9e
Sara <sarazebdeh73@gmail.com>
2023-03-31 15:08:59 +0300

    "TRAIN-49 Protected Routing"

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,6 +9,12 @@ import Home from './components/Home';
 import Unauthorized from './components/Unauthorized';
 import RequireAuth from './components/RequireAuth';
 import Search from './components/university/pages/Search';
+import AcceptedRequests from './components/company/pages/AcceptedRequests';
+import EditTraining from './components/company/pages/EditTraining';
+import Trainers from './components/company/pages/Trainers';
+import TrainingRequests from './components/company/pages/TrainingRequests';
+import CompletedTrainings from './components/Trainer/pages/CompletedTrainings';
+import EvaluationRequests from './components/Trainer/pages/EvaluationRequests';
 import AddStudent from './components/university/pages/AddStudent';
 import AddCompany from './components/university/pages/AddCompany';
 import SubmitRequest from './components/university/pages/SubmitRequest';
@@ -29,16 +35,36 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>
-
         <Route path="/" element={<UNavbar />}></Route>
         {/* <Route element={<RequireAuth allowedRoles={[Roles.UNI_TRAINING_OFFICER]} />} > */}
         <Route path="/submitRequest" element={<SubmitRequest />}></Route>
-        <Route path="/addCompany"    element={<AddCompany />}></Route>
-        <Route path="/addStudent" element={<AddStudent />}></Route> 
+        <Route path="/addCompany" element={<AddCompany />}></Route>
+        <Route path="/addStudent" element={<AddStudent />}></Route>
         {/* <Route path="/completedTrainees" element={<CompletedTrainees />}></Route>
         <Route path="/currentTrainee" element={<CurrentTrainee />}></Route> 
         <Route path="/search" element={<Search />}></Route>
         {/* </Route> */}
+
+       {/* <Route path="/" element={<TNavbar />}></Route> 
+        <Route element={<RequireAuth allowedRoles={[Roles.TRAINER]} />} >
+        <Route path="/completedTrainings" element={<CompletedTrainings />}></Route>
+        <Route path="/evaluationRequests" element={<EvaluationRequests />}></Route>
+        <Route path="/completedTrainees" element={<CompletedTrainees />}></Route>
+        <Route path="/currentTrainee" element={<CurrentTrainee />}></Route> 
+        <Route path="/completedTrainees" element={<CompletedTrainees />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+       </Route> 
+
+        <Route path="/" element={<CNavbar />}></Route>
+       <Route element={<RequireAuth allowedRoles={[Roles.TRAINER]} />} >
+        <Route path="/acceptedRequests" element={<AcceptedRequests />}></Route>
+        <Route path="/editTraining" element={<EditTraining />}></Route>
+        <Route path="/trainers" element={<Trainers />}></Route>
+        <Route path="/completedTrainees" element={<CompletedTrainees />}></Route>
+        <Route path="/currentTrainee" element={<CurrentTrainee />}></Route> 
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/trainingRequests" element={<TrainingRequests />}></Route>
+        </Route> */}
 
       </Routes>
     </QueryClientProvider>
@@ -46,20 +72,3 @@ const App = () => {
 };
 
 export default App;
-
-          {/* <Route path="/" element={<SignIn />} />
-
-          <Route path="unauthorized" element={<Unauthorized />} /> */}
-
-          {/* <Route element={<RequireAuth allowedRoles={[Roles.UNI_TRAINING_OFFICER]} />}>
-           <Route path="addCompany" element={<AddCompany />} />
-           <Route path="submitRequest" element={<SubmitRequest />} />
-           <Route path="completedTrainees" element={<CompletedTrainees />} />
-           <Route path="currentTrainee" element={<CurrentTrainee />} />
-           <Route path="search" element={<Search />} />
-           <Route path="addStudent" element={<AddStudent />} />
-         </Route> */}
-
-          {/* <Route path="*" element={<Missing />} />
-         */}
-       
