c44bc0200ec59d0f91da02f729c28d1ecad6e4ce
shahdamer <shahdamer123456@gmail.com>
2023-04-01 09:17:50 +0300

    TRAIN-45 add company based on branch

--- Backend/src/controllers/companyController.ts
+++ Backend/src/controllers/companyController.ts
@@ -1,60 +1,82 @@
-import { Request, response, Response } from 'express';
-import crypto from 'crypto';
-import  user from '../models/user';
-import userController from './userController';
-import company from '../models/company';
+import { Request, response, Response } from "express";
+import crypto from "crypto";
+import user from "../models/user";
+import userController from "./userController";
+import company from "../models/company";
+import CompanyBranch from "@models/companyBranch";
+class companyController {
+  constructor() {
+    this.addCompany = this.addCompany.bind(this);
+    this.generateAccount = this.generateAccount.bind(this);
+    this.addBranch = this.addBranch.bind(this);
+  }
+  async addCompany(req: Request, res: Response) {
+    const companyId: number = req.body.companyId;
+    const companyName: string = req.body.companyName;
+    const email: string = req.body.email;
+    const location: string = req.body.location;
 
-class companyController{
-    constructor(){
-        this.addCompany=this.addCompany.bind(this);
-        this.generateAccount=this.generateAccount.bind(this);
+    const comp = await company.findByPk(companyId);
+
+      if (!comp) {
+      const id = await this.generateAccount(res, companyName, email, location);
+      if (!id) 
+      return res.json({ msg: "error creating account User" });
+    
+     const record = await company.create({
+      companyId: companyId,
+      companyName,
+      phoneNumber: req.body.phoneNumber,
+      location,
+      managerName: req.body.managerName,
+      id,
+    });
+    if (!record) 
+    return res.json({ msg: "error creating account Company" });
     }
-    async addCompany (req: Request, res: Response) {
-        const companyId:number=req.body.companyId;
-        const  companyName:string = req.body.companyName;
-        const  email:string = req.body.email;
-        const  location:string = req.body.location;
+  const BranchName=await this.addBranch(res,companyId,location)
+  if (!BranchName)
+  return res.json({ msg: "error creating Branch" });
+  
+  return res.json({ msg: "success" })
+}
 
-        const id= await this.generateAccount(res, companyName, email, location)
-        if(!id)
-        res.json({msg:"error creating account User"})
-
-        const record = await company.create({
-            companyId:companyId,
-            companyName,
-            phoneNumber: req.body.phoneNumber,
-            location,
-            managerName: req.body.managerName, 
-            id
-          });
-          if(!record)
-          res.json({msg:"error creating account Company"})
-          res.json({msg:"success creating account Company"})
-        }
-
-    async generateAccount (res: Response,companyName: string,email: string,location: string) { 
-        const first=companyName.split(' ')[0].toLocaleLowerCase();
-        console.log(companyName.split(' '))
-        const second=location.slice(0,2).toLocaleLowerCase();
-        const password = crypto.randomBytes(8).toString('hex'); //random string for password
-        const username=first+'.'+second;
-        var suffix=1;
-        var record = await user.findOne({ where: { username } });
-        var temp=username;
-        console.log("------------------------------------------");
-        console.log(record);
-        if(record){
-            while(record){
-            temp=username+suffix;
-            record = await user.findOne({ where: { username:temp } });
-            suffix++;
-        }
-        }
-        console.log("------------------------------------------");
-        console.log(temp);
-        const id=await userController.addUser(res,temp,password,email,2,6);
-        return id as number;
+  async generateAccount(
+    res: Response,
+    companyName: string,
+    email: string,
+    location: string
+  ) {
+    const first = companyName.split(" ")[0].toLocaleLowerCase();
+    console.log(companyName.split(" "));
+    const second = location.slice(0, 2).toLocaleLowerCase();
+    const password = crypto.randomBytes(8).toString("hex"); //random string for password
+    const username = first + "." + second;
+    var suffix = 1;
+    var record = await user.findOne({ where: { username } });
+    var temp = username;
+    if (record) {
+      while (record) {
+        temp = username + suffix;
+        record = await user.findOne({ where: { username: temp } });
+        suffix++;
+      }
     }
+    const id = await userController.addUser(res, temp, password, email, 2, 6);
+    return id as number;
+  }
 
+  async addBranch(res: Response,companyId:number,location:string) {
+    const Branch =await CompanyBranch.findOne({ where: {location, companyId } });
+    if(Branch)
+    return res.json({ msg: "Company already exists" });
+
+    const BranchName = await CompanyBranch.create({
+      location,
+      companyId
+    })
+    return BranchName
+   
+}
 }
 export default new companyController();
