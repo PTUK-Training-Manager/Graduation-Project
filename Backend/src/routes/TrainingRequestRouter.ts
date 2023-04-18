56fe9581736920e163dac4fee72fc8254040db8f
haneenth <hanendiab11@gmail.com>
2023-04-18 04:32:26 +0200

    TRAIN-64: getTrainingRequests& accept/reject requests

--- Backend/src/routes/TrainingRequestRouter.ts
+++ Backend/src/routes/TrainingRequestRouter.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import RequestController from "../controllers/TrainingRequestController";
+import verifyAccessToken from '../middlewares/verifyAccessToken';
 const router = express.Router();
-const {submitRequest,getPendingRequest,deleteRequest}= RequestController
+const { submitRequest, getPendingRequest, deleteRequest, getTrainingRequest } = RequestController
 
 router.post('/request', submitRequest);
 router.get('/pendingRequests', getPendingRequest);
-router.delete('/request/:id', deleteRequest)
+router.delete('/request/:id', deleteRequest);
+//verify access token?
+router.get('/trainingRequests', verifyAccessToken, getTrainingRequest);
 
 export default router;
