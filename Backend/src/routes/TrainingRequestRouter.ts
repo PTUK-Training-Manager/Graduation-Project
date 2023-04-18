bfae43bd475fc5d257f8a6a080f0b8cf30bcd22d
haneenth <hanendiab11@gmail.com>
2023-04-18 15:18:45 +0200

    TRAIN-64 : format files

--- Backend/src/routes/TrainingRequestRouter.ts
+++ Backend/src/routes/TrainingRequestRouter.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import RequestController from "../controllers/TrainingRequestController";
 import verifyAccessToken from '../middlewares/verifyAccessToken';
 const router = express.Router();
-const { submitRequest, getPendingRequest, deleteRequest, getTrainingRequest,acceptTrainingRequest,rejectTrainingRequest } = RequestController
+const { submitRequest, getPendingRequest, deleteRequest, getTrainingRequest, acceptTrainingRequest, rejectTrainingRequest } = RequestController
 
 router.post('/request', submitRequest);
 router.get('/pendingRequests', getPendingRequest);
 router.delete('/request/:id', deleteRequest);
 //verify access token?
 router.get('/trainingRequests', verifyAccessToken, getTrainingRequest);
-router.get('/accept/:id',  acceptTrainingRequest);
+router.get('/accept/:id', acceptTrainingRequest);
 router.get('/reject/:id', rejectTrainingRequest);
 
 export default router;
