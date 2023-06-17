77a4f9867227a57d921c09bc690f660a30493ed0
Faris Abuali <faris.abuali@outlook.com>
2023-06-17 15:42:04 +0300

    Logoug API - Send access token in req headers

--- Frontend/src/api/logout.ts
+++ Frontend/src/api/logout.ts
@@ -2,7 +2,11 @@ import axiosInstance from "./";
 
 const logout = async () => {
     const url = "/auth/logout";
-    return axiosInstance.post<void>(url).then(res => res.data);
+    return axiosInstance.post<void>(url, {
+        headers: {
+            "access-token": localStorage.getItem("access-token") ?? "",
+        },
+    }).then(res => res.data);
 };
 
 export default logout;
